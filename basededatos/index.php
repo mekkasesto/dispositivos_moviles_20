990a79242d5438ac3f46e8a5dd5259e015f7e703
mekkasesto <mekkasesto@hotmail.com>
2020-12-13 23:00:06 -0600

    login añadido

--- basededatos/index.php
+++ basededatos/index.php
@@ -1,30 +1,46 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--código que incluye Bootstrap-->
-        <?php include'inc/incluye_bootstrap.php' ?>
-        <!--termina código que incluye Bootstrap-->
-        
-        <!-- PARA DATATABLE -->
-        <link href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-        <link href="css/cotizacionesEstilos.css" rel="stylesheet" type="text/css" />        
-    </head>
-    <body>
-        <!--código que incluye el menú responsivo-->
-        <?php include'inc/incluye_menu.php'?>
-        <!--termina código que incluye el menú responsivo-->
-        <div class="container barra">
-            <div class="jumbotron barra">
-            <img src="imgRefacciones/logocopycentro.png" alt="logo">
-            
-                
-            </div>
-        </div>
+<?php
+$error = "";
+if (isset($_GET["msj"])&& ($_GET["msj"] == "msjError")) {
+    $error = "usuario o contaseña incorrectos";
+}
+if (isset($_GET["msj"])&& ($_GET["msj"] == "gracias")) {
+    $error = "enhorabuena vuelve pronto";
+}
+if (isset($_GET["msj"])&& ($_GET["msj"] == "id-denegado")) {
+    $error = "favor de iniciar sesión";
+}
+?>
 
-    </body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="FormularioUsuarios" content="Usuarios">
+
+    <title>InicioIntranet</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="intranet.js"> </script>
+
+
+</head>
+<body>
+
+<div id="toHome">
+    <form action="login/autenticar.php" method="POST" onsubmit="return validacion()"
+    id="inicio" name="inicio">
+        <label for="mail" class="mail">Usuario</label>
+        <input type="text" name="name" id="name"/>
+
+        <label for="pass" class="pass">Contraseña</label>
+        <input type="password" name="pass" id="pass"/>
+        <br class="clearfloat"/>
+       
+        <input type="submit" value="entrar" class="bInicio" id="bInicio"/>
+        <p class="red"><?php echo $error ?></p>
+    </form>
+    
+</div>
+    
+</body>
 </html>
